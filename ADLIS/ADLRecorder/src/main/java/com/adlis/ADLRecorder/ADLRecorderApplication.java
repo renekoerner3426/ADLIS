66434f1b03d845a8332985799ec6185afe5dc701
Lunarion <chris.froemling@volkswagen.de>
2021-01-27 12:45:18 +0100

    trying with mvn around java se

---
+++ ADLIS/ADLRecorder/src/main/java/com/adlis/ADLRecorder/ADLRecorderApplication.java
@@ -0,0 +1,18 @@
+package com.adlis.ADLRecorder;
+
+
+public class ADLRecorderApplication {
+
+	public static void main(String[] args) {
+		ADLRecordExport export = new ADLRecordExport();
+		try {
+			export.sendPost();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
+
+
+}
