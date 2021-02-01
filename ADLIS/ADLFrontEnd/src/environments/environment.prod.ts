f1ca775aaea82940731135c60f5f1fd825fffa15
Lunarion <chris.froemling@volkswagen.de>
2021-02-01 14:39:25 +0100

    basic auth feature

--- ADLIS/ADLFrontEnd/src/environments/environment.prod.ts
+++ ADLIS/ADLFrontEnd/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  basicUrl : '35.222.44.83:8081',
-  accountUrl: '34.122.232.13:8083'
+  basicUrl : '34.121.107.163:8081',
+  accountUrl: '35.224.116.206:8083'
 };
